5839460e8a1733b6c0dbda1fbe4cc9d2f1573fc2
League Student <league@iMac-20.attlocal.net>
2019-08-17 12:57:11 -0700

    fhk
    hjk

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -18,11 +18,28 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
 		Stack<String> bracket = new Stack<String>();
-		
+		for (int i = 0; i < b.length(); i++) {
+			if (b.charAt(i) == '{') {
+				bracket.add("{");
+			} else {
+
+				if (bracket.size() == 0) {
+					return false;
+				} else {
+					bracket.pop();
+				}
+			}
+		}
+
+		if (bracket.size()==0) {
+			return true;
+		}
 		return false;
+
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,69 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener{
 	
+JFrame frame=new JFrame();
+JPanel panel=new JPanel();
+JLabel label=new JLabel();
+int life=20;
+Stack<String> words;
 	
+public static void main(String[] args) {
+
+
+HangMan run=new HangMan();
+run.Setup();
+
+
+}
+
+public void Setup() {
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(label);
+	frame.addKeyListener(this);
+	frame.pack();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
+	Stack<String> words = new Stack<String>();
+	String s= JOptionPane.showInputDialog("Choose a number 1 to 266");
+	int i1= Integer.parseInt(s);
+	for (int i = 0; i < i1; i++) {
+	words.add(Utilities.readRandomLineFromFile("dictionary.txt"));
+	}
+
+	for (int i = 0; i < words.pop().length(); i++) {
+		label.setText(label.getText()+"_ ");
+	
+	}
+	frame.pack();
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
 }
